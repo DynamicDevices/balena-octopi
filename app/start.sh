ad9a6eb2828faa007d6e2ac8f207490b50a07e8c
Eric Betts <bettse@fastmail.fm>
2019-01-20 21:14:19 -0800

    add LD_LIBRARY_PATH export

--- app/start.sh
+++ app/start.sh
@@ -6,8 +6,9 @@ modprobe bcm2835-v4l2 >/dev/null 2>&1 || true
 # Start haproxy
 service haproxy start >/dev/null 2>&1 || true
 
+export LD_LIBRARY_PATH="/usr/src/app/mjpg-streamer/mjpg-streamer-experimental"
 # start picam stream
-cd /usr/src/app/mjpg-streamer/mjpg-streamer-experimental/ && ./mjpg_streamer -i "./input_raspicam.so" -o "./output_http.so -w ./www" -b
+cd /usr/src/app/mjpg-streamer/mjpg-streamer-experimental/ && ./mjpg_streamer -i "input_raspicam.so" -o "output_http.so -w ./www" -b
 
 # start Octoprint
 octoprint serve --iknowwhatimdoing --port=5000 --basedir /data

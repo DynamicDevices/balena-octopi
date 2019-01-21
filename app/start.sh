1d422183844cf6fffa90ecacd3e507fc7c52a675
Eric Betts <bettse@fastmail.fm>
2019-01-20 16:21:09 -0800

    update octoprint start command

--- app/start.sh
+++ app/start.sh
@@ -7,8 +7,7 @@ modprobe bcm2835-v4l2 >/dev/null 2>&1 || true
 service haproxy start >/dev/null 2>&1 || true
 
 # start picam stream
-cd /usr/src/app/mjpg-streamer/mjpg-streamer-experimental/ \
-  && ./mjpg_streamer -i "./input_uvc.so -y" -o "./output_http.so"  &
+cd /usr/src/app/mjpg-streamer/mjpg-streamer-experimental/ && ./mjpg_streamer -i "./input_uvc.so -y" -o "./output_http.so"  &
 
 # start Octoprint
-octoprint --iknowwhatimdoing --port=5000 --basedir /data
+octoprint serve --iknowwhatimdoing --port=5000 --basedir /data
